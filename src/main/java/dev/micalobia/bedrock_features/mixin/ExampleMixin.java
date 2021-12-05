488153d2f710c01176988d901b864dfe2b79aab7
Micah Neitz <micalobia2@gmail.com>
2021-12-05 01:42:38 -0800

    Initial commit

---
+++ src/main/java/dev/micalobia/bedrock_features/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package dev.micalobia.bedrock_features.mixin;
+
+import dev.micalobia.bedrock_features.BedrockFeaturesMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		BedrockFeaturesMod.LOGGER.info("This line is printed by Bedrock Features's ExampleMixin!");
+	}
+}
