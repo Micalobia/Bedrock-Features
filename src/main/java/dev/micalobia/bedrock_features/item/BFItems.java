b008e4cfe7aa9007adc922ab257da1a9b188a71d
Micah <micalobia2@gmail.com>
2021-12-05 22:50:52 -0800

    Made Cakes stack to 64

--- src/main/java/dev/micalobia/bedrock_features/item/BFItems.java
+++ src/main/java/dev/micalobia/bedrock_features/item/BFItems.java
@@ -1,6 +1,7 @@
 package dev.micalobia.bedrock_features.item;
 
 import dev.micalobia.bedrock_features.block.BFBlocks;
+import dev.micalobia.bedrock_features.mixin.item.ItemAccessor;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
@@ -8,5 +9,10 @@ public class BFItems {
 	public static void init() {
 		Item.BLOCK_ITEMS.put(BFBlocks.DYE_CAULDRON, Items.CAULDRON);
 		Item.BLOCK_ITEMS.put(BFBlocks.POTION_CAULDRON, Items.CAULDRON);
+		changeStackSize(Items.CAKE, 64);
+	}
+
+	public static void changeStackSize(Item item, int count) {
+		((ItemAccessor) item).setMaxCount(count);
 	}
 }
