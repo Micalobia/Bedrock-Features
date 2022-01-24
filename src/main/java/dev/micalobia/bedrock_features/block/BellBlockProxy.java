5fa63d738a8fc0b8b636a76bd35db1a2d816d8e6
Micah <micalobia2@gmail.com>
2022-01-24 02:42:32 -0800

    Made Bells observable whilst ringing

---
+++ src/main/java/dev/micalobia/bedrock_features/block/BellBlockProxy.java
@@ -0,0 +1,5 @@
+package dev.micalobia.bedrock_features.block;
+
+public class BellBlockProxy {
+	public static boolean canBeObserved;
+}
---
+++ src/main/java/dev/micalobia/bedrock_features/state/property/BedrockProperties.java
@@ -0,0 +1,7 @@
+package dev.micalobia.bedrock_features.state.property;
+
+import net.minecraft.state.property.BooleanProperty;
+
+public class BedrockProperties {
+	public static BooleanProperty RINGING = BooleanProperty.of("ringing");
+}
